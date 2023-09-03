7966f02712d27a42dd439f42c4057dc291441212
manrick <manricksantos@gmail.com>
2023-09-03 09:23:22 -0300

    inicia a criação da pagina de login

--- src/features/Login/index.tsx
+++ src/features/Login/index.tsx
@@ -1,7 +1,18 @@
+import { Link } from "react-router-dom";
+
 export default function Login(): JSX.Element {
 	return (
 		<>
-			<h1>Login Page</h1>
+			<div className="flex flex-col items-center justify-center h-screen w-full">
+				<Link to="/">
+					<h1 className="mt-11 mb-6 text-white font-bold text-5xl">
+						Dev
+						<span className="bg-gradient-to-r from-yellow-600 to-orange-500 bg-clip-text text-transparent">
+							Link
+						</span>
+					</h1>
+				</Link>
+			</div>
 		</>
 	);
 }
