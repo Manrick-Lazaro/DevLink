2e75fad78176986691097c9e67c1a0c63f2264c8
manrick <manricksantos@gmail.com>
2023-09-16 16:31:45 -0300

    conclui redirecionamento da pagina com base no usuario logado.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,10 +2,23 @@ import { useRoutes } from "react-router-dom";
 import { protectedRoutes } from "./protected";
 import { publicRoutes } from "./public";
 
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../services/firebaseConnection";
+
+import { useState } from "react";
+
 export function Routers(): React.ReactElement | null {
-	const token = localStorage.getItem("token");
-	const routes =
-		token != null ? [...protectedRoutes, ...publicRoutes] : publicRoutes;
+	const [routes, setRoutes] = useState(publicRoutes);
+
+	onAuthStateChanged(auth, (user) => {
+		if (user) {
+			setRoutes([...protectedRoutes, ...publicRoutes]);
+		} else {
+			setRoutes(publicRoutes);
+		}
+	});
+
 	const element = useRoutes([...routes]);
+
 	return element;
 }
